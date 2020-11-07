0a3191d997a8f170728979675fb7c36788fd4c20
Nick Hartjes <info@nickhartjes.nl>
2020-11-07 12:33:04 +0100

    Initial files

---
+++ install-flatpak.sh
@@ -0,0 +1,9 @@
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+flatpak install flathub com.spotify.Client
+flatpak install flathub com.github.tchx84.Flatseal
+flatpak install flathub com.leinardi.gst
+flatpak run com.visualstudio.code
+flatpak install flathub com.microsoft.Teams
+flatpak run com.github.wwmm.pulseeffects
+flatpak install flathub com.github.calo001.fondo
